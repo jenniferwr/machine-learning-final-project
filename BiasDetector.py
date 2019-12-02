862aeaacb2a02b861c736ed218633dca98c4c180
aimenbajwa07 <aimen.bajwa07@hotmail.com>
2019-12-02 18:20:39 -0500

    plots added

--- BiasDetector.py
+++ BiasDetector.py
@@ -5,6 +5,7 @@ import numpy
 from nltk.tokenize import sent_tokenize, word_tokenize
 import warnings
 import itertools
+import matplotlib.pyplot as plt 
 
 
 warnings.filterwarnings(action = 'ignore')
@@ -97,14 +98,175 @@ def getBiasScorePerJobDescription(jobDesc):
         # add to an array that cooresponds to to the same index
         femaleCosineSim.append(femaleAvg)
         maleCosineSim.append(maleAvg)
-        print(".")
+       # print(".")
 
 
     # return an array of the bias scores
     return femaleCosineSim, maleCosineSim
 
+def biasDistribution(jobs,femaleBias,maleBias):
+    #x-axis values 
+    x = jobs
+    # y-axis values 
+    y1 = femaleBias
+    y2 = maleBias
+  
+    # plotting points as a scatter plot 
+    plt.scatter(x, y1, label= "femaleBias", color= "red",  
+            marker= "*", s=30) 
+    plt.ylim(-0.002,0.004)
+    plt.scatter(x, y2, label= "maleBias", color= "green",  
+            marker= "*", s=30) 
+    plt.ylim(-0.002,0.004)
+  
+    # x-axis label 
+    plt.xlabel('Jobs') 
+    # frequency label 
+    plt.ylabel('Bias') 
+    # plot title 
+    plt.title('Distribution of Bias') 
+    # showing legend 
+    plt.legend() 
+  
+    # function to show the plot 
+    plt.show() 
+    return
+
+def biasAndSalaryFreq(jobInfo2, femaleBias, maleBias):
+    x1 = numpy.array(femaleBias)
+    x2 = numpy.array(maleBias)
+
+    unique, rev = numpy.unique(jobInfo2[1:500,6], return_inverse=True)
+    #print (unique)
+    #print (rev)
+    #print (unique[rev])
+
+    fig,ax=plt.subplots()
+    ax.scatter(x1, rev, label= "femaleBias", color= "red", marker= '*')
+    plt.xlim(-0.001,0.004)
+    ax.set_yticks(range(len(unique)))
+    ax.set_yticklabels(unique)
+    plt.plot()
+     # x-axis label 
+    plt.xlabel('Female Bias') 
+    # frequency label 
+    plt.ylabel('Salary Frequency') 
+    # plot title 
+    plt.title('Distribution of Female Bias with reference to Salary Frequency') 
+
+    
+    fig,ax2=plt.subplots()
+    ax2.scatter(x2, rev, label= "maleBias", color= "green", marker= '*')
+    plt.xlim(-0.001,0.004)
+    ax2.set_yticks(range(len(unique)))
+    ax2.set_yticklabels(unique)
+    plt.plot()
+    # x-axis label 
+    plt.xlabel('Male Bias') 
+    # frequency label 
+    plt.ylabel('Salary Frequency') 
+    # plot title 
+    plt.title('Distribution of Male Bias with reference to Salary Frequency') 
+    plt.show()
+    return 
+
+#def biasAndsalary():
+def biasAndFP(jobInfo2, femaleBias, maleBias):
+    x1 = numpy.array(femaleBias)
+    x2 = numpy.array(maleBias)
+
+    unique, rev = numpy.unique(jobInfo2[1:500,3], return_inverse=True)
+    
+    fig,ax=plt.subplots()
+    ax.scatter(x1, rev, label= "femaleBias", color= "red", marker= '*')
+    plt.xlim(-0.001,0.004)
+    ax.set_yticks(range(len(unique)))
+    ax.set_yticklabels(unique)
+    plt.plot()
+     # x-axis label 
+    plt.xlabel('Female Bias') 
+    # frequency label 
+    plt.ylabel('Full-Time/Part-Time') 
+    # plot title 
+    plt.title('Distribution of Female Bias with reference toFull/Part -Time indicator') 
+
+    
+    fig,ax2=plt.subplots()
+    ax2.scatter(x2, rev, label= "maleBias", color= "green", marker= '*')
+    plt.xlim(-0.001,0.004)
+    ax2.set_yticks(range(len(unique)))
+    ax2.set_yticklabels(unique)
+    plt.plot()
+    # x-axis label 
+    plt.xlabel('Male Bias') 
+    # frequency label 
+    plt.ylabel('Full-Time/Part-Time') 
+    # plot title 
+    plt.title('Distribution of Male Bias with reference to Full/Part -Time indicator') 
+    plt.show()
+    return 
+    
+def biasAndJobCat(jobInfo2, femaleBias, maleBias):
+    x1 = numpy.array(femaleBias)
+    x2 = numpy.array(maleBias)
+
+    unique, rev = numpy.unique(jobInfo2[1:500,2], return_inverse=True)
+    
+    fig,ax=plt.subplots()
+    ax.scatter(x1, rev, label= "femaleBias", color= "red", marker= '*')
+    plt.xlim(-0.001,0.004)
+    ax.set_yticks(range(len(unique)))
+    ax.set_yticklabels(unique)
+    plt.plot()
+     # x-axis label 
+    plt.xlabel('Female Bias') 
+    # frequency label 
+    plt.ylabel('Job Category') 
+    # plot title 
+    plt.title('Distribution of Female Bias with reference to Job Category') 
+
+    fig,ax2=plt.subplots()
+    ax2.scatter(x2, rev, label= "maleBias", color= "green", marker= '*')
+    #plt.xlim(-0.001,0.004)
+    ax2.set_yticks(range(len(unique)))
+    ax2.set_yticklabels(unique)
+    plt.plot()
+    # x-axis label 
+    plt.xlabel('Male Bias') 
+    # frequency label 
+    plt.ylabel('Job Category') 
+    # plot title 
+    plt.title('Distribution of Male Bias with reference to Job Category') 
+    plt.show()
+    return
+
+
 def main():
     jobInfo, jobDesc = loadData()
     jobDescArr = numpy.array(jobDesc)
-    getBiasScorePerJobDescription(jobDescArr)
+    femaleBias, maleBias = getBiasScorePerJobDescription(jobDescArr)
+    h=499
+    jobs = [ 0 for x in range(h)]
+    for i in range(len(jobs)):
+        jobs[i] = i
+    biasDistribution(jobs,femaleBias,maleBias)
+    jobInfo2 = numpy.array(jobInfo)  
+    biasAndSalaryFreq(jobInfo2, femaleBias, maleBias)
+    biasAndFP(jobInfo2, femaleBias, maleBias)
+    salaryFrom =jobInfo2[1:500,4]
+    salaryTo = jobInfo2[1:500,5]
+    biasAndJobCat(jobInfo2, femaleBias, maleBias)
+
+    '''avgSalary= []
+    for i in range(len(jobInfo2)):
+        avgSalary[i] = (salaryFrom[i] + salaryTo[i])/2'''
+    
+
+    
+    
+
+
+
+    
+    
 main()
